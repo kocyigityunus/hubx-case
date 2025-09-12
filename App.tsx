d09067819992e3770e9fc57b7528ef02728d9991
Yunus Koçyiğit <kocyigityunus@gmail.com>
2025-09-12 09:13:29 +0300

    redux - temp

--- App.tsx
+++ App.tsx
@@ -6,28 +6,87 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import {
+  Button,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import {
+  configureStore,
+  createAction,
+  createReducer,
+  createSlice,
+} from '@reduxjs/toolkit';
+import { Provider, useSelector } from 'react-redux';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+//
+
+const value = AsyncStorage.getItem('my-key')
+  .then(value => {
+    console.log({ value });
+  })
+  .catch(error => {
+    console.log({ error });
+  });
+
+const appStateSlice = createSlice({
+  name: 'appState',
+  initialState: { isOnboardingCompleted: false },
+  reducers: {
+    markOnboardingAsCompleted: (state, action) => {
+      console.log({ from: 'markOnboardingAsCompleted' });
+      state.isOnboardingCompleted = true;
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: appStateSlice.reducer,
+});
+
+console.log({ from: 'App.tsx' });
+console.log({ store });
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
+  console.log({ from: 'AppContent' });
+  console.log({ store });
 
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+      <Provider store={store}>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <AppContent />
+      </Provider>
     </SafeAreaProvider>
   );
 }
 
 function AppContent() {
   const safeAreaInsets = useSafeAreaInsets();
+  const abc = useSelector((state: RootState) => state);
+  console.log({ abc });
 
   return (
     <View style={styles.container}>
+      <Text style={{ paddingTop: 30 }}>
+        {JSON.stringify(store.getState(), null, 2)}
+      </Text>
+      <Button
+        title="Increment"
+        onPress={() => {
+          store.dispatch(appStateSlice.actions.markOnboardingAsCompleted());
+          console.log({ from: 'line 69' });
+        }}
+      />
       <NewAppScreen
         templateFileName="App.tsx"
         safeAreaInsets={safeAreaInsets}
